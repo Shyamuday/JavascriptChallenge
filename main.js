f17bd54ddf59719d29af598be6bf2b7b6af932c1
Shyam <shyamkumar.jnv@gmail.com>
2023-01-25 17:54:32 +0530

    final commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-showtask();
+showTask();
 let addTaskInput = document.getElementById("addTaskInput");
 let addTaskBtn = document.getElementById("addTaskBtn");
 let addTaskInputval;
@@ -6,45 +6,46 @@ let addTaskInputval;
 addTaskBtn.addEventListener("click", function () {
   addTaskInputval = addTaskInput.value;
   if (addTaskInputval.trim() != 0) {
-    let webTask = localStorage.getItem("showTask");
+    let webTask = localStorage.getItem("localTask");
     if (webTask == null) {
       taskList = [];
     } else {
       taskList = JSON.parse(webTask);
     }
     taskList.push(addTaskInputval);
-    localStorage.setItem("showTask", JSON.stringify(taskList));
+    localStorage.setItem("localTask", JSON.stringify(taskList));
     addTaskInput.value = "";
   }
-  showtask();
+
+  showTask();
 });
 
-function showtask() {
-  let webTask = localStorage.getItem("showTask");
+function showTask() {
+  let webTask = localStorage.getItem("localTask");
   if (webTask == null) {
     taskList = [];
   } else {
     taskList = JSON.parse(webTask);
   }
-  let tabelInnerData = "";
+  let html = "";
   let addedTaskList = document.getElementById("addedTaskList");
   taskList.forEach((item, index) => {
-    tabelInnerData += ` <tr>
+    html += ` <tr>
     <th scope="row">${index + 1}</th>
     <td class="tdItem">${item}</td>
     <td><button class="editbtn" onClick="editTask(${index})" >Edit</button></td>
     <td><button class="deletebtn" onClick="deleteItem(${index})" >Delete</button></td>
 </tr>`;
   });
-  addedTaskList.innerHTML = tabelInnerData;
+  addedTaskList.innerHTML = html;
 }
 
 function editTask(index) {
-  let saveindex = document.getElementById("saveindex");
+  let saveIndex = document.getElementById("saveIndex");
   let addTAskBtn = document.getElementById("addTaskBtn");
   let saveTaskBtn = document.getElementById("saveTaskBtn");
-  saveindex.value = index;
-  let webTask = localStorage.getItem("showTask");
+  saveIndex.value = index;
+  let webTask = localStorage.getItem("localTask");
   let taskList = JSON.parse(webTask);
   addTaskInput.value = taskList[index];
   addTAskBtn.style.display = "none";
@@ -53,30 +54,30 @@ function editTask(index) {
 
 let saveTaskBtn = document.getElementById("saveTaskBtn");
 saveTaskBtn.addEventListener("click", function () {
-  let webTask = localStorage.getItem("showTask");
+  let webTask = localStorage.getItem("localTask");
   let taskList = JSON.parse(webTask);
-  let saveindex = document.getElementById("saveindex").value;
-  taskList[saveindex] = addTaskInput.value;
+  let saveIndex = document.getElementById("saveIndex").value;
+  taskList[saveIndex] = addTaskInput.value;
   saveTaskBtn.style.display = "none";
   addTaskBtn.style.display = "block";
-  localStorage.setItem("showTask", JSON.stringify(taskList));
+  localStorage.setItem("localTask", JSON.stringify(taskList));
   addTaskInput.value = "";
-  showtask();
+  showTask();
 });
 
 function deleteItem(index) {
-  let webTask = localStorage.getItem("showTask");
+  let webTask = localStorage.getItem("localTask");
   let taskList = JSON.parse(webTask);
   taskList.splice(index, 1);
-  localStorage.setItem("showTask", JSON.stringify(taskList));
-  showtask();
+  localStorage.setItem("localTask", JSON.stringify(taskList));
+  showTask();
 }
 
 let deleteAllBtn = document.getElementById("deleteAllBtn");
 deleteAllBtn.addEventListener("click", function () {
   let saveTaskBtn = document.getElementById("saveTaskBtn");
   let addTaskBtn = document.getElementById("addTaskBtn");
-  let webTask = localStorage.getItem("showTask");
+  let webTask = localStorage.getItem("localTask");
   let taskList = JSON.parse(webTask);
   if (webTask == null) {
     taskList = [];
@@ -87,13 +88,13 @@ deleteAllBtn.addEventListener("click", function () {
 
   saveTaskBtn.style.display = "none";
   addTaskBtn.style.display = "block";
-  localStorage.setItem("showTask", JSON.stringify(taskList));
-  showtask();
+  localStorage.setItem("localTask", JSON.stringify(taskList));
+  showTask();
 });
 
 let searchTextBox = document.getElementById("searchTextBox");
 searchTextBox.addEventListener("input", function () {
-  let trList = document.querySelectorAll("tr");
+  let trList = document.querySelector("tr");
   Array.from(trList).forEach(function (item) {
     let serchedText = item.getElementsByTagName("td")[0].innerText;
     let searchTextBoxval = searchTextBox.value;
